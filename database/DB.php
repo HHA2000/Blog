c736771e4591cc88397de43901f9ba88467139a0
Wizard <daweiwizard2000@gmail.com>
2020-11-16 14:14:59 +0630

    add index

--- database/DB.php
+++ database/DB.php
@@ -14,62 +14,12 @@
         }
 
         public function index() {
-            $statement = $this->pdo->prepare("SELECT * FROM todo");
-
-            if($statement->execute()) {
-                for($count = 0; $count < $statement->columnCount(); $count++) {
-                    $column_meta = $statement->getColumnMeta($count);
-                    $column_names[] = $column_meta['name'];
-                }
-
-                $datum = $statement->fetchall(PDO::FETCH_CLASS);
-
-                return [$column_names, $datum];
-            }
-        }
-
-        public function edit($id) {
-            $statement = $this->pdo->prepare("SELECT * FROM todo WHERE id = :id");
-            $statement->bindParam(":id", $id);
-
+            $statement = $this->pdo->prepare("SELECT p.title, p.content, p.image, p.created_at, u.name 
+            FROM posts as p 
+            INNER JOIN users as u
+            ON p.author_id = u.id");
             if ($statement->execute()) {
-                $result = $statement->fetch(PDO::FETCH_OBJ);
-                return $result;
-            }
-        }
-
-        public function delete($id) {
-            $statement = $this->pdo->prepare("DELETE FROM todo WHERE id = :id");
-            $statement->bindParam(":id", $id);
-
-            if ($statement->execute()) {
-                header("location:index.php");
-            }
-        }
-
-        public function update($id, $title, $description) {
-            $statement = $this->pdo->prepare("UPDATE `todo` 
-            SET `title` = :title, `description` = :description 
-            WHERE id = :id");
-
-            $statement->bindParam(":id", $id);
-            $statement->bindParam(":title", $title);
-            $statement->bindParam(":description", $description);
-
-            if($statement->execute()) {
-                header("location:index.php");
-            }
-        }
-
-        public function store($title, $description) {
-            $statement = $this->pdo->prepare("INSERT INTO `todo` (`title`, `description`) 
-            VALUE(:title, :description)");
-
-            $statement->bindParam(":title", $title);
-            $statement->bindParam(":description", $description);
-
-            if($statement->execute()) {
-                header("location:index.php");
+                return $statement->fetchall(PDO::FETCH_CLASS);
             }
         }
 
---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+  require_once "database/DB.php";
+  $db = new DB();
+  $blogs = $db->index();
+?>
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>AdminLTE 3 | Starter</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="">
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand navbar-white navbar-light">
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+  </nav>
+  <!-- /.navbar -->
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Blog</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <?php foreach ($blogs as $blog): ?>
+              <div class="col-md-4">
+                <!-- Box Comment -->
+                <div class="card card-widget">
+                  <div class="card-header">
+                    <div class="user-block">
+                      <span class="admin_name"><a href="#"><?php echo $blog->name?></a></span><br>
+                      <span><?php echo $blog->created_at ?></span>
+                    </div>
+                    <!-- /.user-block -->
+                  </div> 
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <h3><?php echo $blog->title ?></h3>
+
+                    <img class="img-fluid pad" src="<?php echo $blog->image ?>" alt="">
+
+                    <p><?php echo $blog->content ?></p>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
